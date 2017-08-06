9881500b227479b06beaaea9c775b8078f6bcd19
Eliot Logan <eliot.logan@gmail.com>
2017-08-05 18:55:12 -0700

    testing theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -5,6 +5,10 @@ from __future__ import unicode_literals
 AUTHOR = u'Eliot Logan'
 SITENAME = u'skewed'
 SITEURL = 'http://bikeage.github.io'
+#SITEURL = ''
+
+HEADER_COVER = 'static/my_image.png'
+HEADER_COLOR = 'red'
 
 PATH = 'content'
 
@@ -26,14 +30,22 @@ LINKS = (('Pelican', 'http://getpelican.com/'),
          ('Jinja2', 'http://jinja.pocoo.org/'),
          ('You can modify those links in your config file', '#'),)
 
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
 
-DEFAULT_PAGINATION = 1
+
+# Social widget
+SOCIAL = (('Facebook', 'http://facebook.com/eliotlogan'),
+          ('github', 'https://github.com/myprofile'),
+          ('Twitter', 'http://twitter.com/eliotlogan'),
+          ('envelope','mailto:my@mail.address')
+          )
+
+STATIC_PATHS = ['assets']
+
+
+DEFAULT_PAGINATION = 3
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
 
 THEME = 'attila'
 
@@ -41,3 +53,20 @@ MARKUP = ('md', 'ipynb')
 
 PLUGIN_PATH = './plugins'
 PLUGINS = ['ipynb.markup']
+
+### Theme specific settings
+
+COLOR_SCHEME_CSS = 'github.css'
+
+CSS_OVERRIDE = ['assets/css/myblog.css']
+
+AUTHORS_BIO = {
+  "zutrinken": {
+    "name": "Zutrinken",
+    "cover": "assets/images/avatar.png",
+    "image": "assets/images/avatar.png",
+    "website": "http://blog.arulraj.net",
+    "location": "Chennai",
+    "bio": "This is the place for a small biography with max 200 characters. Well, now 100 are left. Cool, hugh?"
+  }
+}
